993d6a41eec0bd0c8c8c3c43b8e63dff4a097807
ethandmd <ethandmd@protonmail.com>
2023-03-10 00:15:44 -0800

    Add redis cnxn pool and json parsing for /run post requests

--- broker/src/main.rs
+++ broker/src/main.rs
@@ -1,37 +1,70 @@
 use std::env;
 
-use actix_web::{HttpServer, HttpRequest, App, get, post, Responder};
+use serde::Deserialize;
+use actix_web::{HttpServer, HttpResponse, App, get, post, web, Responder, Result};
+use deadpool_redis::{redis::cmd, Config, Runtime, CreatePoolError};
+
+#[derive(Deserialize)]
+struct SimArgs {
+    name: String,
+}
+
+fn redis_new() -> Result<deadpool_redis::Pool, CreatePoolError> {
+    let url = env::var("REDIS").expect("Required: Broker redis url");
+    let cfg = Config::from_url(url);
+    let runtime = Some(Runtime::Tokio1);
+
+    cfg.create_pool(runtime)
+}
 
 #[post("/load")]
 async fn load() -> impl Responder {
     "Broker load route."
 }
 
-#[get("/run")]
-async fn run() -> impl Responder {
-    "Broker run route."
+#[post("/run")]
+async fn run(pool: web::Data<deadpool_redis::Pool>, json: web::Json<SimArgs>) -> impl Responder {
+    println!("{}", json.name);
+    let mut conn = pool.get().await.unwrap();
+
+    match cmd("SET")
+        .arg(&["test-key", &json.name])
+        .query_async::<_, ()>(&mut conn)
+        .await {
+            Ok(_) => HttpResponse::Ok(),
+            Err(_) => HttpResponse::BadRequest(),
+        }
 }
 
 #[get("/")]
-async fn index(req: HttpRequest) -> impl Responder {
-    println!("{:?}", req);
+async fn index() -> impl Responder {
     "Broker index route."
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    
+    let host = env::var("HOST")
+        .expect("Required: Broker server host.");
+
     let port = env::var("PORT")
         .expect("Required: Broker server port.")
         .parse::<u16>()
         .expect("Required: Broker server port must be u16.");
 
-    HttpServer::new(|| {
+    // TODO: Actix middleware logging.
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
+    let pool = web::Data::new(redis_new().unwrap());
+    HttpServer::new(move || {
         App::new()
+            .app_data(pool.clone())
             .service(index)
             .service(run)
             .service(load)
     })
-    .bind(("0.0.0.0", port))?
+    .bind((host, port))?
     .run()
     .await
 }
