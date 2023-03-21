83fc0db1d509f4fb5ba4d8781f1a41cc20f79a2e
ethandmd <ethandmd@protonmail.com>
2023-03-21 10:47:54 -0700

    Update types fields to reflect api spec

--- broker/src/types.rs
+++ broker/src/types.rs
@@ -2,23 +2,23 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Species {
-    starting_umber: u8,
-    size: u8,
-    speed: u8,
-    sight: u8,
-    health: u8,
-    cohesion: u8,
-    aggression: u8,
+    starting_umber: u32,
+    size: u32,
+    speed: u32,
+    sight: u32,
+    health: u32,
+    cohesion: f32,
+    aggression: f32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SimInput {
-    num_timesteps: u16, // Limit 1, 1000
-    view_granularity: u16, // Limit 1, 1000
-    cache_views: u16,
-    world_size_x: u8,
-    world_size_y: u8,
-    food_density: u32,
+    num_timesteps: u32, // Limit 1, 1000
+    view_granularity: u32, // Limit 1, 1000
+    cache_views: bool,
+    world_size_x: u32,
+    world_size_y: u32,
+    food_density: f32,
     species_list: Vec<Species>,
 }
 
